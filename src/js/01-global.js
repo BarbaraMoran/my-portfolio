a35954d59c0fabc66115c535fafc0cba23cc208d
BarbaraMoran <barbaramoranrr@gmail.com>
2021-09-24 16:33:29 +0200

    add button event

--- src/js/01-global.js
+++ src/js/01-global.js
@@ -1,8 +1,12 @@
 "use strict";
 
-const formEl = document.querySelector(".js-form");
-const inputEl = document.querySelector(".js-input");
-const btnEl = document.querySelector(".js-button");
-const showsListTitle = document.querySelector(".results-section-title");
-const showsList = document.querySelector(".js-list");
-const favShowsList = document.querySelector(".js-fav-list");
+const burgerEl = document.querySelector(".js-nav");
+const sectionBtnEls = document.querySelectorAll(".js-sectionBtn");
+
+function handleButton(event) {
+  for (const sectionBtnEl of sectionBtnEls) {
+    sectionBtnEl.classList.toggle("hidden");
+  }
+}
+
+burgerEl.addEventListener("click", handleButton);
