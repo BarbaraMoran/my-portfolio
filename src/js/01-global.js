a07924ecb3d93e4c6120d096d1a9558b44270a4a
BarbaraMoran <barbaramoranrr@gmail.com>
2021-09-27 11:29:12 +0200

    add links

--- src/js/01-global.js
+++ src/js/01-global.js
@@ -2,11 +2,14 @@
 
 const burgerEl = document.querySelector(".js-nav");
 const sectionBtnEls = document.querySelectorAll(".js-sectionBtn");
+const linkListEl = document.querySelector(".js-list");
 
 function handleButton(event) {
   for (const sectionBtnEl of sectionBtnEls) {
     sectionBtnEl.classList.toggle("hidden");
   }
+
+  linkListEl.classList.toggle("hidden");
 }
 
 burgerEl.addEventListener("click", handleButton);
